61008fd25d6b1bc4dfe742353f82a150bc231eae
RaviKant <rk3727000@gmail.com>
2024-10-26 20:02:20 +0545

    Added rajan Code JobSeeker Profile Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import Navbar from './Component/Navbar.jsx';
 import Footer from './Component/LandingPage/Footer.jsx';
 import './App.css'
 import HeroSection from './Component/LandingPage/HeroSection.jsx';
+import JobSeekerProfile from './Component/Profiles/JobSeekerProfile.jsx';
 
 function App() {
 
   return (
     <>
-      <Navbar/>
+      {/* <Navbar/>
       <HeroSection/>
       <br/>
       <hr/>
       <br/>
-      <Footer/>
-
+      <Footer/> */}
+  <JobSeekerProfile/>
     </>
   )
 }
