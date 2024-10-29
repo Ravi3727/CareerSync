eceea2bb5de33b5d727015167ee003fab0009fcb
Sonal-ai <sonalkhanak@gmail.com>
2024-10-29 12:27:23 +0530

    no change

--- src/Component/Auth/RecruiterRegistration.jsx
+++ src/Component/Auth/RecruiterRegistration.jsx
@@ -60,7 +60,7 @@ const RecruiterRegistration = () => {
              style={{ backgroundImage: `url(${backgroundImage})` }}>
             <div className='h-5/6 w-1/3 bg-white p-1 rounded-2xl shadow-2xl shadow-sky-300'>
                 <form className='h-full w-full overflow-y-auto px-7' onSubmit={onSubmit}> 
-                    <h1 className='text-3xl text-center p-5 font-semibold text-sky-500'>Recruiter Registration</h1>
+                    <h1 className='text-3xl text-center p-5 font-semibold text-blue-500'>Recruiter Registration</h1>
                     <div className='flex flex-col justify-center items-center'>
 
                         {/* Hidden file input */}
@@ -76,7 +76,7 @@ const RecruiterRegistration = () => {
                         {/* Circular icon or image button */}
                         <label 
                             htmlFor="profilePhoto" 
-                            className="flex items-center justify-center w-40 h-40 border-2 border-sky-500 rounded-full mb-4 cursor-pointer hover:bg-sky-100 transition duration-200"
+                            className="flex items-center justify-center w-40 h-40 border-2 border-blue-500 rounded-full mb-4 cursor-pointer hover:bg-sky-100 transition duration-200"
                         >
                             {profileImage ? (
                                 <img
@@ -91,39 +91,39 @@ const RecruiterRegistration = () => {
                     </div>
 
                     {/* Input Fields */}
-                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-sky-500 rounded-md'>
+                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-blue-500 rounded-md'>
                         <label htmlFor="name" className='absolute -top-4 ml-4 bg-inherit px-2'>Name*</label>
                         <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} required className='m-2 border-collapse focus:outline-none text-sm ' />
                     </div>
-                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-sky-500 rounded-md'>
+                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-blue-500 rounded-md'>
                         <label htmlFor="email" className='absolute -top-4 ml-4 bg-inherit px-2'>Company email*</label>
                         <input type="email" id='email' name="email" value={email} onChange={(e) => setEmail(e.target.value)} required className='m-2 border-collapse focus:outline-none text-sm'/>
                     </div>
-                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-sky-500 rounded-md'>
+                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-blue-500 rounded-md'>
                         <label htmlFor="email" className='absolute -top-4 ml-4 bg-inherit px-2'>Password*</label>
                         <input type="password" id='password' name="passwrod" value={password} onChange={(e) => setPassword(e.target.value)} required className='m-2 border-collapse focus:outline-none text-sm'/>
                     </div>
-                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-sky-500 rounded-md'>
+                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-blue-500 rounded-md'>
                         <label htmlFor="phone" className='absolute -top-4 ml-4 bg-inherit px-2'>Phone no.*</label>
                         <input type="tel" id='phone' name="phone" value={phone} onChange={(e) => setPhone(e.target.value)} required className='m-2 border-collapse focus:outline-none text-sm'/>
                     </div>
-                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-sky-500 rounded-md'>
+                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-blue-500 rounded-md'>
                         <label htmlFor="company_name" className='absolute -top-4 ml-4 bg-inherit px-2'>Company Name*</label>
                         <input type="text" id='company_name' name="company_name" value={companyName} onChange={(e) => setCompanyName(e.target.value)} required className='m-2 border-collapse focus:outline-none text-sm'/>
                     </div>
-                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-sky-500 rounded-md'>
+                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-blue-500 rounded-md'>
                         <label htmlFor="company_web" className='absolute -top-4 ml-4 bg-inherit px-2'>Company Website*</label>
                         <input type="url" id='company_web' name="company_web" value={companyWeb} onChange={(e) => setCompanyWeb(e.target.value)} required className='m-2 border-collapse focus:outline-none text-sm'/>
                     </div>
-                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-sky-500 rounded-md'>
+                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-blue-500 rounded-md'>
                         <label htmlFor="designation" className='absolute -top-4 ml-4 bg-inherit px-2'>Designation*</label>
                         <input type="text" id='designation' name="designation" value={designation} onChange={(e) => setDesignation(e.target.value)} required className='m-2 border-collapse focus:outline-none text-sm'/>
                     </div>
-                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-sky-500 rounded-md'>
+                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-blue-500 rounded-md'>
                         <label htmlFor="linkedin" className='absolute -top-4 ml-4 bg-inherit px-2'>Linkedin*</label>
                         <input type="url" id='linkedin' name="linkedin" value={linkedin} onChange={(e) => setLinkedin(e.target.value)} required className='m-2 border-collapse focus:outline-none text-sm'/>
                     </div>
-                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-sky-500 rounded-md'>
+                    <div className='flex flex-col my-4 relative bg-white border-solid border-2 border-blue-500 rounded-md'>
                         <label htmlFor="text" className='absolute -top-4 ml-4 bg-inherit px-2'>Description</label>
                         <textarea id='text' name="text" rows="5" value={text} onChange={(e) => setText(e.target.value)} className='m-2 border-collapse focus:outline-none text-sm'/>
                     </div>
@@ -132,7 +132,7 @@ const RecruiterRegistration = () => {
                         <input type="checkbox" name="terms" required />
                         <span className="ml-2">I agree to the Terms and Conditions*</span>
                     </div>
-                    <button type="submit" className='bg-sky-500 p-2 rounded-md w-40 m-3 mb-6'>Submit</button>
+                    <button type="submit" className='bg-blue-500 p-2 rounded-md w-40 m-3 mb-6 text-blue-50'>Submit</button>
                 </form>
                 <div>
                     {/* Display submitted data */}
